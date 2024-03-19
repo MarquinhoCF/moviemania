21a9e4cb0d4e157c94b89a7df8bcff0fa377599e
MarquinhoCF <marcos13@gmail.com>
2024-03-19 19:17:13 -0300

    Registro de usuário

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -1,15 +1,18 @@
 <?php
 
     require_once("models/User.php");
+    require_once("models/Message.php");
 
     class UserDAO implements UserDAOInterface {
 
         private $conn;
         private $url;
+        private $message;
 
         public function __construct(PDO $conn, $url) {
             $this->conn = $conn;
             $this->url = $url;
+            $this->message = new Message($url);
         }
 
         public function buildUser($data){
@@ -30,7 +33,24 @@
         }
 
         public function create(User $user, $authUser = false){
+            $stmt = $this->conn->prepare("INSERT INTO users (
+                name, lastname, email, password, token
+            ) VALUES (
+                :name, :lastname, :email, :password, :token
+            )");
 
+            $stmt->bindParam(":name", $user->name);
+            $stmt->bindParam(":lastname", $user->lastname);
+            $stmt->bindParam(":email", $user->email);
+            $stmt->bindParam(":password", $user->password);
+            $stmt->bindParam(":token", $user->token);
+
+            $stmt->execute();
+
+            // Autenticar usuário caso $authUser for true
+            if ($authUser) {
+                $this->setTokenToSession($user->token);
+            }
         }
 
         public function update(User $user){
@@ -46,7 +66,13 @@
         }
 
         public function setTokenToSession($token, $redirect = true){
+            // Salvar o Token na sessão
+            $_SESSION["token"] = $token;
 
+            if ($redirect) {
+                // Redireciona para o perfil do usuário
+                $this->message->setMessage("Seja bem-vindo!", "success", "editprofile.php");
+            }
         }
 
         public function authenticateUser($email, $password){
---
+++ editprofile.php
@@ -0,0 +1,15 @@
+<?php 
+
+    require_once("templates/header.php")
+
+?>
+
+    <div id="main-container" class="container-fluid">
+        <h1>Edição de Perfil</h1>    
+    </div>
+
+<?php 
+
+    require_once("templates/footer.php")
+
+?>
